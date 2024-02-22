20970219da2925c86c35973a468e6d0dc98b785a
asad-tyb0000 <asad.tyb0000@gmail.com>
2024-02-22 20:32:24 +0500

    week 4

---
+++ PD 4/task2/BL/Player.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalDuel.BL
+{
+    internal class Player
+    {
+        public string playerName;
+        public double Health;
+        private double currentHealth;
+        private double maxHealth;
+        private double energy;
+        private double maxEnergy;
+        private double armor;
+        private Stats skillStats;
+
+        public Player(string playerName, double Health, double energy, double armor)
+        {
+            this.playerName = playerName;
+            this.Health = Health;
+            this.energy = energy;
+            this.armor = armor;
+        }
+
+        public void UpdateHealth(double amount)
+        {
+            currentHealth += amount;
+            if (currentHealth < 0)
+                currentHealth = 0;
+            else if (currentHealth > maxHealth)
+                currentHealth = maxHealth;
+        }
+
+        public void UpdateEnergy(double amount)
+        {
+            energy += amount;
+            if (energy < 0)
+                energy = 0;
+            else if (energy > maxEnergy)
+                energy = maxEnergy;
+        }
+
+        public void UpdateArmor(double amount)
+        {
+            armor += amount;
+            if (armor < 0)
+                armor = 0;
+        }
+
+        public void LearnSkill(Stats stats)
+        {
+            skillStats = stats;
+        }
+
+        public string Attack(Player target)
+        {
+            if (skillStats == null)
+            {
+                return $"{playerName} attempted to attack, but hasn't learned any skill yet!";
+            }
+
+            if (energy < skillStats.Cost)
+            {
+                return $"{playerName} attempted to use {skillStats.Name}, but didn't have enough energy!";
+            }
+
+            energy -= skillStats.Cost;
+
+            double effectiveArmor = Math.Max(0, target.armor - skillStats.Penetration);
+            double calculatedDamage = skillStats.Damage * ((100 - effectiveArmor) / 100);
+
+            target.UpdateHealth(-calculatedDamage);
+            UpdateHealth(skillStats.Heal);
+
+            string result = $"{playerName} used {skillStats.Name}, {skillStats.Description}, against {target.playerName}, doing {calculatedDamage} damage!";
+
+            if (skillStats.Heal > 0)
+            {
+                result += $" {playerName} healed for {skillStats.Heal} health!";
+            }
+
+            if (target.currentHealth <= 0)
+            {
+                result += $" {target.playerName} died.";
+            }
+            else
+            {
+                double targetHpPercentage = (target.currentHealth / target.maxHealth) * 100;
+                result += $" {target.playerName} is at {targetHpPercentage:F2}% health.";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ PD 4/task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using MagicalDuel.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicalDuel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player mage1 = new Player("Elena", 110, 50, 10);
+            Player mage2 = new Player("Derek", 100, 60, 20);
+
+            Stats fireball = new Stats("fireball", 23, "a fiery magical attack", 1.2, 15, 5);
+            mage1.learnSkill(fireball);
+            Console.WriteLine(mage1.attack(mage2));
+
+            Stats superbeam = new Stats("superbeam", 200, "an overpowered attack, pls nerf", 50, 50, 75);
+            mage2.learnSkill(superbeam);
+            Console.WriteLine(mage2.attack(mage1));
+
+            Console.ReadKey();
+        }
+    }
+}
